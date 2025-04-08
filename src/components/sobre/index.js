03903c9e6fbfe9a4b6f6334ceca9f33440df4f5c
braziledu19 <brazileduardo1909@gmail.com>
2025-04-07 23:58:49 -0300

    qualquer coisa

---
+++ src/components/sobre/index.js
@@ -0,0 +1,21 @@
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
+export default function Cabeçalho() {
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={4} sx={{ backgroundColor: "red" }}>
+            <Typography variant="h6">AutoWeb</Typography>
+          </Grid>
+
+          
+
+
+        </Grid>
+      </Box>
+    </>
+  );
+}
